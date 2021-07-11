140c5984dc40164d8bca37f2159cdee1d2ca99dd
MaykonFranchini <maykonfranchini2017@gmail.com>
2021-07-11 04:44:36 +0100

    search bar and studio cards style done

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,17 +7,18 @@ class StudiosController < ApplicationController
             else 
                 @studios = @studio
             end
+            @markers = @studios.geocoded.map do |studio|
+                {
+                  lat: studio.latitude,
+                  lng: studio.longitude,
+                  info_window: render_to_string(partial: "/studios/info_window", locals: { studio: studio }),
+                  image_url: helpers.asset_url('camera.png')
+                }
+              end
         end
         
     
         # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-        @markers = @studios.geocoded.map do |studio|
-          {
-            lat: studio.latitude,
-            lng: studio.longitude,
-            info_window: render_to_string(partial: "/studios/info_window", locals: { studio: studio }),
-            image_url: helpers.asset_url('camera.png')
-          }
-        end
+        
     end
 end
